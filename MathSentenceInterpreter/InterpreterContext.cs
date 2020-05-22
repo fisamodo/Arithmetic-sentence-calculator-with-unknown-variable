270ba1f6de2e124202097dc0e4ed7cf991361bb4
fisamodo <fisamodo@vsite.hr>
2020-05-22 12:14:06 -0700

    Added class constructor, planning to make basic math parser next

---
+++ MathSentenceInterpreter/InterpreterContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathSentenceInterpreter
+{
+    public class InterpreterContext
+    {
+        string s;
+        double x;
+        public InterpreterContext(String s, double x)
+        {
+            this.s = s;
+            this.x = x;
+        }
+    }
+}
