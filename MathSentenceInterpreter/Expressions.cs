4788ac67bc6551bec65a5d8fe29369b2273dbacd
fisamodo <fisamodo@vsite.hr>
2020-05-22 12:02:20 -0700

    Added abstract class and  defined abstract functions

---
+++ MathSentenceInterpreter/Expressions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathSentenceInterpreter
+{
+    public abstract class Expressions
+    {
+        public abstract string BasicMathExpression(string s, double x = 0.01, int flag = -1);
+        public abstract string BasicMathExpressionWithOneOrMoreUnknown(string s, double x, int flag = -2);
+        public abstract string BasicMathExpressionWithTrigonometry(string s, double x);
+    }
+}
---
+++ MathSentenceInterpreter/MathExpressions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathSentenceInterpreter
+{
+    public class MathExpressions : Expressions
+    {
+        public override string BasicMathExpression(string s, double x = 0.01, int flag = -1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string BasicMathExpressionWithOneOrMoreUnknown(string s, double x, int flag = -2)
+        {
+
+            throw new NotImplementedException();
+
+
+        }
+
+        public override string BasicMathExpressionWithTrigonometry(string s, double x)
+        {
+            throw new NotImplementedException();
+
+        }
+    }
+}
